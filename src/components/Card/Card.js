8848415a2cc8f34632c7ce65384b41bcc5aad210
buicuong13031998 <62436724+buicuong13031998@users.noreply.github.com>
2020-07-09 15:12:03 +0700

    countdown and text overflow

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,42 +1,61 @@
-import React from "react";
-import "./Card.scss";
+import './Card.scss';
+import React, { useEffect, useState, useRef } from 'react';
 
-class Card extends React.PureComponent {
-  render() {
-    const { dataItem } = this.props;
-    return (
-      <div className="card">
-        <div className="ctn-img">
-          <img src={dataItem.imageUrl} />
-        </div>
-        <div className="ctn-name">
-          <img
-            width={15}
-            height={15}
-            src="https://dummyimage.com/200x200/000/fff"
-          />
-          <span className="txt-name">{dataItem.name}</span>
-        </div>
-        <div className="quantity-item">1 Items</div>
-        <div className="quantity-panto">
-          <img
-            width={15}
-            height={15}
-            src="https://dummyimage.com/200x200/000/fff"
-          />
-          <span className="txt-panto">{dataItem.quantity} PANTO POINT</span>
-        </div>
-        <div className="time-remaining">
-          <img
-            width={15}
-            height={15}
-            src="https://dummyimage.com/200x200/000/fff"
-          />{" "}
-          <span className="txt-time"> {dataItem.time} </span>
-        </div>
-      </div>
-    );
-  }
+function formartTime(seconds) {
+  if (!seconds) return;
+  console.log(seconds);
+  let hours = Math.floor(seconds / 3600);
+
+  seconds = seconds % 3600;
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+function Card(props) {
+  const { dataItem } = props;
+  let [time, setTime] = useState('');
+  const timeRef = useRef(dataItem.time);
+  useEffect(() => {
+    const clockInterval = setInterval(() => {
+      const newTime = timeRef.current - 1;
+      if (newTime >= 0) {
+        setTime(formartTime(newTime));
+        timeRef.current = newTime;
+        console.log(timeRef.current);
+      }
+    }, 1000);
+    return () => {
+      console.log('clean up clock');
+      clearInterval(clockInterval);
+    };
+  }, []);
+  return (
+    <div className="card">
+      {timeRef.current > 1 ? (
+        <>
+          <div className="ctn-img">
+            <img src={dataItem.imageUrl} />
+          </div>
+          <div className="ctn-name">
+            <img width={15} height={15} src="https://dummyimage.com/200x200/000/fff" />
+            <span className="txt-name">{dataItem.name}</span>
+          </div>
+          <div className="quantity-item">1 Items</div>
+          <div className="quantity-panto">
+            <img width={15} height={15} src="https://dummyimage.com/200x200/000/fff" />
+            <span className="txt-panto">{dataItem.quantity} PANTO POINT</span>
+          </div>
+          <div className="time-remaining">
+            <img width={15} height={15} src="https://dummyimage.com/200x200/000/fff" />{' '}
+            <span className="txt-time"> {time} </span>
+          </div>
+        </>
+      ) : (
+        ''
+      )}
+    </div>
+  );
 }
 
 export default Card;
--- src/components/ListCard/ListCard.js
+++ src/components/ListCard/ListCard.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Card from "../Card/Card";
-import "./ListCard.scss";
+import React from 'react';
+import Card from '../Card/Card';
+import './ListCard.scss';
 
 class ListCard extends React.Component {
   constructor(props) {
@@ -9,37 +9,37 @@ class ListCard extends React.Component {
       dataListCard: [
         {
           id: 1,
-          imageUrl: "https://dummyimage.com/600x400/000/fff",
-          name: "test1",
+          imageUrl: 'https://dummyimage.com/600x400/000/fff',
+          name: 'test1',
           quantity: 20,
-          time: 60,
+          time: 5,
         },
         {
           id: 2,
-          imageUrl: "https://dummyimage.com/600x200/000/fff",
-          name: "test2",
+          imageUrl: 'https://dummyimage.com/600x200/000/fff',
+          name: 'test2',
           quantity: 30,
           time: 120,
         },
         {
           id: 3,
-          imageUrl: "https://dummyimage.com/200x400/000/fff",
-          name: "1xddQuY3p163eRhQgRDqEs4N5NMyxUXQEbDEew6AZdLQ",
+          imageUrl: 'https://dummyimage.com/200x400/000/fff',
+          name: '1xddQuY3p163eRhQgRDqEs4N5NMyxUXQEbDEew6AZdLQ',
           quantity: 10,
           time: 120,
         },
         {
           id: 4,
-          imageUrl: "https://dummyimage.com/200x100/000/fff",
-          name: "test4",
+          imageUrl: 'https://dummyimage.com/200x100/000/fff',
+          name: 'test4',
           quantity: 50,
           time: 250,
         },
         {
           id: 5,
-          imageUrl: "https://dummyimage.com/200x200/000/fff",
+          imageUrl: 'https://dummyimage.com/200x200/000/fff',
           name:
-            "Những chiếc máy ảnh bé xinh được chúng mình tự tay thiết kế và sản xuất, dựa trên những mẫu máy ảnh film trên thực tế",
+            'Những chiếc máy ảnh bé xinh được chúng mình tự tay thiết kế và sản xuất, dựa trên những mẫu máy ảnh film trên thực tế',
           quantity: 40,
           time: 500,
         },
